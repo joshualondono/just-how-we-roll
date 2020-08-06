fcb59d50073f3b6be8e19b92658c98ec015d970d
joshua londono <joshualondono@outlook.com>
2020-08-06 12:01:24 -0400

    have reset funcitonal, contorl over image elements, and adding value to averages

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
  * DATA *
  **********/
 
-const sixes = [];
-const doubleSixes = [];
-const twelves = [];
-const twenties = [];
+let sixes = [6,7,8,8];
+let doubleSixes = [22,5,4];
+let twelves = [];
+let twenties = [];
+
 
 
 /********************
@@ -16,7 +17,7 @@ const getRandomNumber = function(max) {
     const rand = Math.random();
     const range = rand * max;
     const result = Math.ceil(range);
-    
+
     return result;
 }
 
@@ -29,8 +30,24 @@ const sortByNumber = function(arr) {
 }
 
 /*******************
- * YOUR CODE BELOW *
+ * const doubleMedian = document.querySelectorAll("#double-d6-rolls-median");
+ doubleMedian[0].textContent='test';
+ doubleMedian.appendChild(document.createTextNode('Median'));
+
+ const doubleMode = document.querySelectorAll("#double-d6-rolls-mode");
+ doubleMode[0].textContent=doubleSixes;
+ doubleMode.appendChild(document.createTextNode('Mode')); *
  *******************/
+//adding values
+
+const first = document.querySelectorAll("#double-d6-rolls-median");
+first[0].textContent=doubleSixes;
+
+const second = document.querySelectorAll("#double-d6-rolls-mean");
+second[0].textContent=doubleSixes;
+
+const third = document.querySelectorAll("#double-d6-rolls-mode");
+third[0].textContent=doubleSixes;
 
 
 
@@ -38,9 +55,54 @@ const sortByNumber = function(arr) {
  * EVENT LISTENERS *
  *******************/
 
+ const dice4 = document.querySelector("#d20-roll")
+ dice4.addEventListener('click', function(event) {
+     dice4.style.backgroundColor = "red"
+   })
+
+
+const dice3 = document.querySelector("#d12-roll")
+   dice3.addEventListener('click', function(event) {
+       dice3.style.backgroundColor = "blue"
+     })
+
+const dice2 = document.querySelector("#double-d6-roll-1")
+const dice1 = document.querySelector("#double-d6-roll-2")
+     dice2.addEventListener('click', function(event) {
+         dice2.style.backgroundColor = "black"
+         dice1.style.backgroundColor = "black"
+       })
+
+const dice = document.querySelector("#d6-roll")
+          dice.addEventListener('click', function(event) {
+          dice.style.backgroundColor = "black"
+
+})
+
+
+
 /******************
  * RESET FUNCTION *
  ******************/
+
+   const buttons = document.querySelectorAll("button")
+for (const button of buttons) {
+  button.addEventListener('click', function(event) {
+
+    sixes = [];
+    doubleSixes = [];
+    twelves = [];
+    twenties = [];
+
+    first[0].textContent=doubleSixes;
+    second[0].textContent=doubleSixes;
+    third[0].textContent=doubleSixes;
+
+
+}
+  )
+}
+
 
 /****************
  * MATH SECTION *
